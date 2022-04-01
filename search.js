cfaa78910ea0b47180f6845604aa6b30e1b298a1
Prathmesh112000 <87255237+Prathmesh112000@users.noreply.github.com>
2022-04-01 19:22:43 +0530

    search option added

---
+++ search.js
@@ -0,0 +1,19 @@
+var arr1=JSON.parse(localStorage.getItem("skincare_science"))
+var arr2=JSON.parse(localStorage.getItem("blush_data"))
+var arr3=JSON.parse(localStorage.getItem("scents"))
+var arr4=JSON.parse(localStorage.getItem("hair_prod"))
+var arr5=JSON.parse(localStorage.getItem("bundle_joy"))
+
+var main_arr=[]
+main_arr.push(...arr1,...arr2,...arr3,...arr4,...arr5)
+
+var search_item=localStorage.getItem("search_item")
+
+var result=[]
+
+main_arr.map(function(elem){
+    if(elem.name==search_item){
+        result.push(elem)
+    }
+})
+console.log(result)
